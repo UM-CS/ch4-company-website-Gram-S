c89fb73c211667940c84c8842d07766f699e9d39
Gram-S <grantms4455@gmail.com>
2025-10-05 16:14:34 -0600

    Make a really bad and slightly unethical store

--- pages/views.py
+++ pages/views.py
@@ -17,3 +17,14 @@ class AboutPageView(TemplateView):
 		context["contact_address"] = "123 Cool Guy Street"
 		context["phone_number"] = "111-222-3333"
 		return context
+
+class ProductsPageView(TemplateView):
+    template_name = "products.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Product_A"] = "Helium-3 Isotope |"
+        context["Product_B"] = "Fresh Hay |"
+        context["Product_C"] = "Tires (1 left in stock |
+        context["Product_D"] = "My stupid iphone charger that makes a weird buzzing sound whenever i plug it in |"
+        return context
